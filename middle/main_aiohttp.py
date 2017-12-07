b82ac87825d19e88ce5032431543e28fd09d2cad
Caio Ramos Casimiro <caiorcasimiro@gmail.com>
2017-12-07 00:21:46 -0200

    a bunch of changes

--- middle/main_aiohttp.py
+++ middle/main_aiohttp.py
@@ -7,17 +7,16 @@ import aiohttp.web
 
 UPSTREAM_HOST = os.environ['UPSTREAM_HOST']
 URL = 'http://%s:8080' % UPSTREAM_HOST
+SESSION = aiohttp.ClientSession()
 
 
 async def handle(request):
     payload = await request.content.read()
-    async with aiohttp.ClientSession() as session:
-        with async_timeout.timeout(30):
-            async with session.post(URL, data=payload) as response:
-                response_data = await response.text()
-                return aiohttp.web.Response(text=response_data)
+    with async_timeout.timeout(30):
+        async with SESSION.post(URL, data=payload) as response:
+            return aiohttp.web.Response(text=await response.text())
 
-app = aiohttp.web.Application()
+app = aiohttp.web.Application(debug=False)
 app.router.add_post('/', handle)
 
 aiohttp.web.run_app(app)
--- provider/main_aiohttp.py
+++ provider/main_aiohttp.py
@@ -5,7 +5,7 @@ import aiohttp.web
 async def handle(request):
     return aiohttp.web.Response(text='{"inserted_id": "asdfasdf"}')
 
-app = aiohttp.web.Application()
+app = aiohttp.web.Application(debug=False)
 app.router.add_post('/', handle)
 
 aiohttp.web.run_app(app)
